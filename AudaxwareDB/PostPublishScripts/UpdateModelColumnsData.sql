2371c8211c0735d50fb3470d50cab8df70fd5057
rafael.toledo <rafael.toledo@audaxware.com>
2025-07-08 21:34:06 +0000

    Merged PR 92: feat: Update post publish
    Create a query to update and synchronize newly added columns.
    
    Related work items: #4362, #4432

--- AudaxwareDB/PostPublishScripts/UpdateModelColumnsData.sql
+++ AudaxwareDB/PostPublishScripts/UpdateModelColumnsData.sql
@@ -13,22 +13,50 @@ Post-Deployment Script Template
 exec sp_set_session_context 'domain_id', '1';
 
 GO
-
-
-    UPDATE User_gridView 
-    SET grid_state = REPLACE(CAST(grid_state AS VARCHAR(MAX)), 'serial_', 'model_')
-    UPDATE project_room_inventory SET serial_number = null;
-
-
---IF EXISTS (SELECT 1 FROM assets WHERE model_number IS NULL) 
---BEGIN
---    UPDATE assets 
---    SET model_number = serial_number, model_name = serial_name;
     
---    UPDATE project_room_inventory 
---    SET model_number = serial_number, model_name = serial_name, model_number_ow = serial_number_ow, model_name_ow = serial_name_ow;
+BEGIN TRY
+    BEGIN TRANSACTION;
 
---END 
+    -- Update User_gridView
+    UPDATE User_gridView 
+    SET grid_state = REPLACE(CAST(grid_state AS VARCHAR(MAX)), 'serial_', 'model_');
+
+    -- Update project_room_inventory
+
+    DISABLE TRIGGER ALL ON project_room_inventory;
+
+    UPDATE project_room_inventory
+    SET
+        model_number    = serial_number,
+        model_number_ow = serial_number_ow,
+        model_name      = serial_name,
+        model_name_ow   = serial_name_ow
+    WHERE
+        COALESCE(serial_name, '') <> COALESCE(model_name, '')
+        OR COALESCE(serial_number, '') <> COALESCE(model_number, '');
+
+    ENABLE TRIGGER ALL ON project_room_inventory;
+
+    -- Update Assets
+    DISABLE TRIGGER ALL ON assets;
+
+    UPDATE assets
+    SET
+        model_number = serial_number,
+        model_name   = serial_name
+    WHERE
+        COALESCE(serial_name, '') <> COALESCE(model_name, '')
+        OR COALESCE(serial_number, '') <> COALESCE(model_number, '');
+
+    ENABLE TRIGGER ALL ON assets;
+
+    COMMIT;
+
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+END CATCH;
+
 
 /*
 TODO LATER: REMOVE COLUMNS AND DATA FROM TABLE
